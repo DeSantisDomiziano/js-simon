5e120a3211c8f84e1599b92e3bf26d07070d0681
DeSantisDomiziano <desantis.domiziano@gmail.com>
2023-03-21 18:01:37 +0100

    add timer and field

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,29 +7,46 @@ Invece di usare prompt e allerte usate inputs ed elementi della dom per mostrare
 */
 
 const START_BUTTON_EL = document.getElementById('start')
+const SEND_BUTTON_EL = document.getElementById('send')
+const FORM_EL = document.getElementById('campo-invio')
 const FIELD_GAME_EL = document.querySelector('.col-12.text-center')
 const TIMER_EL = document.getElementById('timer')
 const RANDOM_NUMBER = []
+const USER_NUMBER = []
+
 
 START_BUTTON_EL.addEventListener('click', function(){
+
+    let div_number_el = [];
 
     while(RANDOM_NUMBER.length < 5) {
         const generator_of_number = Math.floor(Math.random() * 100)
         if(!RANDOM_NUMBER.includes(generator_of_number)) {
             RANDOM_NUMBER.push(generator_of_number)
-            const div_number_el = document.createElement('div')
-            div_number_el.classList.add('col', 'text-center')
-            div_number_el.innerHTML = `${generator_of_number}`
-            FIELD_GAME_EL.insertAdjacentElement('afterend', div_number_el)
+
+            const item_div_number =  document.createElement('div')
+            item_div_number.classList.add('col', 'text-center')
+            item_div_number.innerHTML = `${generator_of_number}`
+
+            div_number_el.push(item_div_number)
+
+            FIELD_GAME_EL.insertAdjacentElement('afterend', item_div_number)
         }
     }
 
-    setTimeout(myFunction, 30000)
+    setTimeout(myFunction, 3000)
 
     function myFunction() {
+        for(let i = 0; i < 5; i++) {
+            div_number_el[i].innerHTML = ''
+        }
+        TIMER_EL.innerHTML= 'TEMPO SCADUTO!! <br> ora scrivi un numero alla volta ed invia'
 
-        TIMER_EL.innerHTML= 'TEMPO SCADUTO!!'
-    }
+        FORM_EL.classList.remove('d-none')        
+    }   
     
-    
+})
+
+SEND_BUTTON_EL.addEventListener('click', function (event) {
+    event.preventDefault()
 })
